e9fb67cafdf2e0b71332339ab72382276d64264e
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-05-15 23:25:09 +0200

    Remove PATH from config locations.

--- functions/RESTServiceCommand.config.js
+++ functions/RESTServiceCommand.config.js
@@ -2,13 +2,13 @@ var SERVICE_URL = process.env.SERVICE_URL ? process.env.SERVICE_URL : "https://l
 
 var STORAGE = process.env.STORAGE ? process.env.STORAGE : "aws";
 var BUCKET = process.env.BUCKET ? process.env.BUCKET : "bucket";
-var PATH = process.env.PATH ? process.env.PATH : "data"; //prefix in a bucket with no leading or trailing slashes
+var DATA_PATH = process.env.DATA_PATH ? process.env.DATA_PATH : "data"; //prefix in a bucket with no leading or trailing slashes
 
 exports.service_url = SERVICE_URL;
 
 exports.options = {
     "storage": STORAGE,
     "bucket": BUCKET,
-    "prefix": PATH
+    "prefix": DATA_PATH
 };
 
